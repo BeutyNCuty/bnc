35bdc6b45800819dad3f474881e35a2046e5c4fb
mumura <js_0811@naver.com>
2022-05-09 15:31:47 +0900

    initialize project

---
+++ src/main/java/com/bnc/main/BncApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.bnc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BncApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BncApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnc/main/BncApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnc.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BncApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
