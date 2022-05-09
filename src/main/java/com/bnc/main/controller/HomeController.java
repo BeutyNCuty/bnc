6a1cc513714e8a22c0db49c0e2321b5337d80d3d
mumura <js_0811@naver.com>
2022-05-09 23:55:51 +0900

    Add HomeController slack test PM 10 : 36

--- src/main/java/com/bnc/main/controller/HomeController.java
+++ src/main/java/com/bnc/main/controller/HomeController.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
     //테스트
-    string test = "우왕 진짜 안되네";
+    string test = "이제 되려나?";
 }
