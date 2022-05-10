19163a861cf260b73dcc324580e073787243696e
naruhoto0 <likefeeling@hanmail.net>
2022-05-10 14:29:49 +0900

    BEUT-16 상품 도메인 설계

---
+++ src/test/java/com/bnc/main/product/domain/ProductTest.java
@@ -0,0 +1,51 @@
+package com.bnc.main.product.domain;
+
+import com.google.common.base.Preconditions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+//assertj의 assertThat은 인자로 actual(검증대상)만 받는다.
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class ProductTest {
+
+    @Test
+    void 상품_생성_성공() {
+        final Product product = new Product("옷", 123, "구찌");
+        assertThat(product.getName()).isEqualTo("옷");
+        assertThat(product.getPrice()).isEqualTo(123);
+        assertThat(product.getBrand()).isEqualTo("구찌");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 상품_이름이_null이면_실패(String name){
+        assertThatIllegalArgumentException().isThrownBy(() -> new Product(name, 123, "구찌"));
+    }
+
+    @Test
+    void 상품_이름이_공백이면_실패(){
+        String name = "    ";
+        assertThatIllegalArgumentException().isThrownBy(() -> new Product(name, 123, "구찌"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 상품_브랜드가_null이면_실패(String brand){
+        assertThatIllegalArgumentException().isThrownBy(() -> new Product("옷", 123, brand));
+    }
+
+    @Test
+    void 상품_브랜드가_공백이면_실패(){
+        String brand = "    ";
+        assertThatIllegalArgumentException().isThrownBy(() -> new Product("옷", 123, brand));
+    }
+
+    @Test
+    void 상품_기본가격이_0원이면_성공(){
+        Product product = new Product();
+        assertThat(product.getPrice()).isEqualTo(0);
+    }
+}
