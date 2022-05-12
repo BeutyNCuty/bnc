6bd16498b3aaea57e07deeb9949743a66596b2ef
mumura <js_0811@naver.com>
2022-05-12 10:39:25 +0900

    BEUT-19 유저 도메인 개발 멤버 커밋 누락 오류 해결

--- src/main/java/com/bnc/main/member/domain/Grade.java
+++ src/main/java/com/bnc/main/member/domain/Grade.java
@@ -1,9 +1,9 @@
 package com.bnc.main.member.domain;
 
 public enum Grade {
-    Gold,   // 51만원이상
-    Silver, // 21만원 ~ 50만원
-    Bronze  // 0원부터 ~ 20만원
+    Gold,
+    Silver,
+    Bronze
 }
     /*
     Gold    51만원이상
