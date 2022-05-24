ceef6fac08b63d60b442f226d2d2242895221314
yeji jeong <jy98102@naver.com>
2022-05-24 17:47:08 +0900

    BEUT-35 플로우 설명

--- src/main/java/com/bnc/main/controller/HomeController.java
+++ src/main/java/com/bnc/main/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.bnc.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -15,7 +14,7 @@ public class HomeController {
     public String hello(){
         return "hello";
     }
-
+//asd
 
 
 }
