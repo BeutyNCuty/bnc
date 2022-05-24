329c7ec448d2338d45f9a5ece40ed47ebb086f2c
yeji jeong <jy98102@naver.com>
2022-05-24 16:18:07 +0900

    BEUT-33 테스트

--- src/main/java/com/bnc/main/controller/HomeController.java
+++ src/main/java/com/bnc/main/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.bnc.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
